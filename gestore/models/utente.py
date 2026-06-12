5ce553df53a50980d7469f9ac21298dbf54f3322
GabrielePittori <gpittori668@gmail.com>
2026-06-12 20:36:28 +0200

    Aggiunta (rifatta) dei primi modelli

--- gestore/models/utente.py
+++ gestore/models/utente.py
@@ -1,1 +1,20 @@
-# Prova di scrittura sul file utente che dovrà essere rinominato con estensione .py
+class Utente:
+    def __init__(self, nome: str, cognome: str, eta: int, email: str, password: str, codice_identificativo: str):
+        self._nome = nome
+        self._cognome = cognome
+        self._eta = eta
+        self._email = email
+        self._password = password
+        self._codice_identificativo = codice_identificativo
+        self._preferenze = []
+
+    # L'UNICA FUNZIONE: Recupera tutti i dati dell'utente in un colpo solo
+    def ottieni_profilo_completo(self) -> dict:
+        return {
+            "nome": self._nome,
+            "cognome": self._cognome,
+            "eta": self._eta,
+            "email": self._email,
+            "codice": self._codice_identificativo,
+            "preferenze": self._preferenze
+        }
