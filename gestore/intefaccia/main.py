aad1d1291e3ee1f1a40ce4c5ce7a05db10572d0e
msilviarota <msilviarota@gmail.com>
2026-06-13 09:30:30 +0200

    creato il file main.py per interfaccia
    creato il file main.py per interfaccia

--- gestore/intefaccia/main.py
+++ gestore/intefaccia/main.py
@@ -1,1 +1,9 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from login import LoginWindow
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    finestra = LoginWindow()
+    finestra.show()
+    sys.exit(app.exec())
