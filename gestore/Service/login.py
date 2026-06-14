c99710a2d3d10868cae62a8fd775dc8de3ab7d68
GabrielePittori <gpittori668@gmail.com>
2026-06-14 13:35:55 +0200

    Aggiunta seconda bozza del GestoreAccessi.

--- gestore/Service/login.py
+++ gestore/Service/login.py
@@ -1,15 +1,44 @@
-class GestoreLogin():
+from gestore.models import notifica
+
+class GestoreAccessi():
 
     def __init__(self, db):
         self.db = db
 
-    def getModulo(self):
-        return ["email", "password"] # In un caso reale, questo potrebbe essere un oggetto più complesso o un template HTML
+    def getModulo(self, modulo):
+        match modulo:
+            case "login":
+                return ["email", "password"]
+            case "CambioPassword":
+                return ["vecchia_password", "nuova_password", "conferma_nuova_password"]
+            case _:
+                return []
     
     def inviaCredenziali(self, email, password):
         print(f"[Control] Ricevuti dati: {email}. Verifica in corso...")
         return self.login(email, password)
+    
+    def criptaPassword(self, password):
+        passwordCriptata = "hashed_" + password  # Simulazione di hashing
+        return passwordCriptata
 
+    def bloccaOperazione(self, errore: str):
+        notifica.inviaErrore(errore)
+        print("[Control] Operazione bloccata per motivi di sicurezza.")
+        return False
+    
+    def mostra(self, esitoVerifica: str):
+        match esitoVerifica:
+            case "successo":
+                print("[Control] Accesso consentito.")
+            case "Utente non trovato":
+                self.bloccaOperazione("Utente non trovato")
+            case "password errata":
+                self.bloccaOperazione("password errata")
+            case _:
+                print("[Control] Accesso negato.")
+        return
+    
     def login(self, email, password):
         user = self.db.get_user_by_email(email)
         if user and user.check_password(password):
