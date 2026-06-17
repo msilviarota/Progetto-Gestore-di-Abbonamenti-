8ac6b05695bda15a7e9e2a1d7f6ad92c3596131b
GabrielePittori <gpittori668@gmail.com>
2026-06-17 10:33:03 +0200

    Creazione del model Preferenza e della RepositoryPreferenze. Rinomina del file ricerca a gestoreRicerca.

--- gestore/models/preferenza.py
+++ gestore/models/preferenza.py
@@ -1,1 +1,8 @@
+from models.utente import Utente
 
+class Preferenza:
+    def __init__(self, utente: Utente):
+        self._preferenze = utente.get_preferenze()
+
+    def getCategorie(self):
+        return self._preferenze
