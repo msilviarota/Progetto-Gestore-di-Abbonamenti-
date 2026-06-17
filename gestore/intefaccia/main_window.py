ed5b6356b7eb1b903c0a1e1d6d77db030292356c
msilviarota <msilviarota@gmail.com>
2026-06-17 10:44:48 +0200

    aggiunti riga 198,199,200

--- gestore/intefaccia/main_window.py
+++ gestore/intefaccia/main_window.py
@@ -195,20 +195,6 @@ class FinestraPrincipale(QWidget):
 
     def filtra_categorie(self):
         from dialoghi import FinestraRicerca
-        testo = self.campo_ricerca.text().lower().strip()
-
-        if not testo:
-            return
-
-        risultati = []
-        for chiave, servizi in self.link_categorie.items():
-            if testo in chiave.lower():
-                for nome, link, _ in servizi:
-                    risultati.append((f"{chiave} → {nome}", link))
-            else:
-                for nome, link, _ in servizi:
-                    if testo in nome.lower():
-                        risultati.append((nome, link))
-
-        finestra = FinestraRicerca(risultati, self)
+        testo = self.campo_ricerca.text().strip()
+        finestra = FinestraRicerca (testo, self)
         finestra.exec()
