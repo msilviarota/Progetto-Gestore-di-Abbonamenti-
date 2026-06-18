c91bb3c5953c428616f02712e1dcb8a5cb4cea52
msilviarota <msilviarota@gmail.com>
2026-06-18 19:21:52 +0200

    Scrittura del codice contenuto

--- gestore/models/contenuto.py
+++ gestore/models/contenuto.py
@@ -1,16 +1,14 @@
 class Contenuto:
-    """Rappresenta un contenuto audiovisivo (film, serie tv, ecc.) delle piattaforme."""
-    def _init_(self, id_contenuto: str, titolo: str, piattaforma: str, tipologia: str):
+    """
+    Rappresenta un contenuto audiovisivo (film, serie tv, ecc.) delle piattaforme.
+    """
+    
+    # Corretto: rinominato da *init* a __init__ per la corretta inizializzazione
+    def __init__(self, id_contenuto: str, titolo: str, piattaforma: str, tipologia: str):
+        """
+        Inizializza un oggetto Contenuto con i dati provenienti dal catalogo.
+        """
         self._id_contenuto = id_contenuto
         self._titolo = titolo
         self._piattaforma = piattaforma
         self._tipologia = tipologia
-
-    def ottieni_dettagli_contenuto(self) -> dict:
-        """Recupera i dettagli del contenuto multimediale."""
-        return {
-            "id": self._id_contenuto,
-            "titolo": self._titolo,
-            "piattaforma": self._piattaforma,
-            "tipologia": self._tipologia
-        }
