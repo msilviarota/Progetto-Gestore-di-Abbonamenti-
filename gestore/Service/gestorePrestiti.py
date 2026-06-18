a75d40b27604bc10d444f32f2ae212ffcfb52c6a
msilviarota <msilviarota@gmail.com>
2026-06-18 16:15:57 +0200

    aggiustato file

--- gestore/Service/gestorePrestiti.py
+++ gestore/Service/gestorePrestiti.py
@@ -1,6 +1,6 @@
-from database.repositoryUtente import RepositoryUtente
-from database.repositoryLog import RepositoryLog
-from database.repositoryAbbonamento import RepositoryAbbonamento
+from repository.repositoryUtente import RepositoryUtente
+from repository.repositoryLog import RepositoryLog
+from repository.repositoryAbbonamento import RepositoryAbbonamento
 from models.notifica import Notifica
 
 
@@ -8,33 +8,43 @@ class GestorePrestiti:
     def __init__(self, 
                  repo_utente: RepositoryUtente,
                  repo_abbonamento: RepositoryAbbonamento,
-                 repo_log: RepositoryLog,  # Iniettato per una migliore testabilità
+                 repo_log: RepositoryLog,  
                  notifica: Notifica):
         
         self._repo_utente = repo_utente
         self._repo_abbonamento = repo_abbonamento
         self._repo_log = repo_log
         self._notifica = notifica
-        
-       
-        self._email_utente = self._repo_log.recuperaUltimoLog()
     
-    def avvia_prestito(self, email_amico: str, id_abbonamento: int):
+    def avvia_prestito(self, email_amico: str,nome_piattaforma: int):
         """
         Avvia la procedura di condivisione di un abbonamento con un amico.
         """
         # 1. Verifica se l'amico esiste nel sistema
-        if not self._repo_utente.verifica(email_amico):
-            self._notifica.inviaErrore("Il tuo amico/a deve avere un account Gestore")
+        email_utente = self._repo_log.recuperaUltimoLog()
+        if not email_utente:
+            self._notifica.inviaErrore("Errore: sessione utente non trovata.")
+            return None
+      
+        id_abbonamento = self._repo_abbonamento.get_id_by_nome(nome_piattaforma)
+        
+        if id_abbonamento is None:
+            self._notifica.inviaErrore(f"Abbonamento {nome_piattaforma} non trovato.")
             return None
 
-        # 2. Condivide l'abbonamento duplicando i permessi
-        self._repo_abbonamento.duplicaPermessiAccesso(
-            self._email_utente, 
+        # 3. Verifica se l'amico esiste
+        if not self._repo_utente.verifica(email_amico):
+            self._notifica.inviaErrore("Il tuo amico/a deve avere un account registrato.")
+            return None
+
+        # 4. Condivisione
+        successo = self._repo_abbonamento.duplicaPermessiAccesso(
+            email_utente, 
             email_amico, 
             id_abbonamento
         )
         
-        # 3. Invia la notifica di successo e la restituisce
-        notifica_ritorno = self._notifica.invia("Abbonamento condiviso correttamente")
-        return notifica_ritorno
+        if successo:
+            return self._notifica.invia("Abbonamento condiviso correttamente")
+        
+        return None
