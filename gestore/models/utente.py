4d8eca6dbf620b008d53ba298f6a3ceddabcf172
msilviarota <msilviarota@gmail.com>
2026-06-18 19:17:14 +0200

    Scrittura del codice utente

--- gestore/models/utente.py
+++ gestore/models/utente.py
@@ -1,26 +1,9 @@
 class Utente:
+    """Rappresenta l'utente registrato al gestore."""
+    
     def __init__(self, nome: str, cognome: str, eta: int, email: str, password: str):
         self._nome = nome
         self._cognome = cognome
         self._eta = eta
         self._email = email
         self._password = password
-
-    def get_nome(self): return self._nome
-
-    def get_cognome(self): return self._cognome
-
-    def get_eta(self): return self._eta
-
-    def get_email(self): return self._email
-
-    def get_password(self): return self._password
-
-    def to_dict(self):
-        return {
-            "nome": self._nome,
-            "cognome": self._cognome,
-            "eta": self._eta,
-            "email": self._email,
-            "password": self._password,
-        }
