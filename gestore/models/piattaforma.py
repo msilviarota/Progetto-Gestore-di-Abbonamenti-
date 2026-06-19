cab8265bb7b7b5042cdf9e222004964a430ff5a1
msilviarota <msilviarota@gmail.com>
2026-06-19 18:27:19 +0200

    prova2

--- gestore/models/piattaforma.py
+++ gestore/models/piattaforma.py
@@ -62,11 +62,7 @@ CATALOGO_PIATTAFORME = {
         link_login="https://www.netflix.com/login",
         logo="loghi/netflix.png",
         categoria="Streaming",
-         catalogo=[
-            Contenuto("n1", "Stranger Things", "Netflix", "Serie"),
-            Contenuto("n2", "The Witcher", "Netflix", "Serie"),
-            Contenuto("n3", "Red Notice", "Netflix", "Film"),
-        ]
+     
     ),
     "prime video": Piattaforma(
         nome="Prime Video",
