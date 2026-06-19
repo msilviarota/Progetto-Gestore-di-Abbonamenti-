0aaefb24e18b212a3daeadd157efc30213bda42a
msilviarota <msilviarota@gmail.com>
2026-06-19 14:53:26 +0200

    tolto le modifiche

--- gestore/intefaccia/main.py
+++ gestore/intefaccia/main.py
@@ -17,9 +17,7 @@ from models.notifica import Notifica
 from Service.gestoreLogin import GestoreLogin
 from Service.gestorePreferenze import GestorePreferenze
 from intefaccia.main_window import FinestraPrincipale
-from Service.gestoreAbbonamenti import GestoreAbbonamenti
-from repository.repositoryAbbonamento import RepositoryAbbonamento
-from repository.repositoryDatiPagamento import RepositoryDatiPagamento
+
 from models.notifica import Notifica
 def avvia_applicazione():
     app = QApplication(sys.argv)
@@ -34,20 +32,10 @@ def avvia_applicazione():
     # Nota: GestorePreferenze richiede anche un GestoreRicerca; per ora None
     # (è usato solo in un punto non ancora implementato nel backend)
     gestore_preferenze = GestorePreferenze(repo_utente, repo_preferenze, None, notifica)
-    # --- Gestore Abbonamenti ---
-    repo_abbonamenti = RepositoryAbbonamento()
-    repo_pagamenti = RepositoryDatiPagamento()
 
-# ATTENZIONE: la piattaforma la imposterai dopo nella finestra Acquista
-    gestore_abbonamenti = GestoreAbbonamenti(
-    utente=None,  # verrà impostato dopo il login
-    repoAbbonamento=repo_abbonamenti,
-    repoDatiPagamento=repo_pagamenti,
-    piattaforma=None,
     notifica=notifica
-)
 
-    window = LoginWindow(gestore_login=gestore_login, gestore_preferenze=gestore_preferenze,gestore_profilo=gestore_profilo,gestore_abbonamenti=gestore_abbonamenti)
+    window = LoginWindow(gestore_login=gestore_login, gestore_preferenze=gestore_preferenze,gestore_profilo=gestore_profilo)
     window.show()
     sys.exit(app.exec())
 
