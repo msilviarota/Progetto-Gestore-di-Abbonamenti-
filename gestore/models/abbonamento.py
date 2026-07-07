93c8089adf2f67253615153a7186b893b3727743
msilviarota <msilviarota@gmail.com>
2026-07-07 15:39:28 +0200

    aggiunto un nuovo piano di abbonamenti

--- gestore/models/abbonamento.py
+++ gestore/models/abbonamento.py
@@ -3,12 +3,17 @@ from datetime import datetime, timedelta
 class Abbonamento:
     """Rappresenta un abbonamento acquistato (CDU1)."""
     
-    def __init__(self, email: str, nome_utente: str, cognome_utente: str, piattaforma_nome: str):
+    def __init__(self, email: str, nome_utente: str, cognome_utente: str, 
+                 piattaforma_nome: str, piano: str = "mensile"):
         self._email = email
         self._nome_utente = nome_utente
         self._cognome_utente = cognome_utente
         self.piattaforma = piattaforma_nome
-        # La scadenza è impostata di default a 365 giorni (Stagionale)
-        self._data_scadenza = datetime.now() + timedelta(days=365)
+        self._piano = piano
+
+        # Scadenza calcolata in base al piano scelto
+        giorni_validita = 365 if piano == "annuale" else 30
+        self._data_scadenza = datetime.now() + timedelta(days=giorni_validita)
+
         self._validita = True
         self._stato = "Attivo"
